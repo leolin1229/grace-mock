947174a9e4b7b0052024088d5002edb1dcf355b2
leolin1229 <leolin1229@gmail.com>
2017-06-12 15:24:51 +0800

    transform to es5

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-import Mock from 'mockjs';
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _mockjs = require('mockjs');
+
+var _mockjs2 = _interopRequireDefault(_mockjs);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function serialize(str) {
-  const paramArray = str.split('&');
-  const query = {};
-  paramArray.forEach((item) => {
+  var paramArray = str.split('&');
+  var query = {};
+  paramArray.forEach(function (item) {
     query[item.split('=')[0]] = item.split('=')[1];
   });
 
   return query;
 }
 
-export default {
-  ...Mock,
-  $convert(apiObject) {
-    Object.keys(apiObject).forEach((key) => {
-      Mock.mock(eval(`/${key.split(' ')[1].replace(/\//g, '\\/')}/`), key.split(' ')[0].toLowerCase(), (options) => { // eslint-disable-line no-eval
-        let query = { options };
+exports.default = _extends({}, _mockjs2.default, {
+  $convert: function $convert(apiObject) {
+    Object.keys(apiObject).forEach(function (key) {
+      _mockjs2.default.mock(eval('/' + key.split(' ')[1].replace(/\//g, '\\/') + '/'), key.split(' ')[0].toLowerCase(), function (options) {
+        // eslint-disable-line no-eval
+        var query = { options: options };
         if (key.split(' ')[0].toLowerCase() === 'get') {
           if (options.url.split('?')[1]) {
             query = serialize(options.url.split('?')[1]);
@@ -23,15 +35,15 @@ export default {
             query = options.body ? JSON.parse(options.body) : {};
           }
         }
-        const res = {};
-        let result = {};
+        var res = {};
+        var result = {};
         res.json = function ret(data) {
           result = data;
         };
-        apiObject[key]({ ...options, query }, res);
+        apiObject[key](_extends({}, options, { query: query }), res);
 
         return result;
       });
     });
-  },
-};
+  }
+});
